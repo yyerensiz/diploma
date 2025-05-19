ffe47bac3d1e555efa3abb6cb7c78742bedb0119
yyerensiz <s01t002208@gmail.com>
2025-05-19 15:12:51 +0500

    carenest predefense 3 v1

--- backend/ztempedSRC/controllers/config.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-module.exports = {
-    PORT: process.env.PORT || 5000,
-    JWT_SECRET: process.env.JWT_SECRET,
-    DATABASE_URL: process.env.DATABASE_URL,
-    FIREBASE_CREDENTIALS: process.env.FIREBASE_CREDENTIALS,
-};
