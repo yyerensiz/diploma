c7e9b37e448ca1ed8748416580eed6d82a7b4eba
yyerensiz <s01t002208@gmail.com>
2025-06-07 17:42:05 +0500

    CareNest project - Final Defense

--- front_client/libNew/core/constants.dart
+++
@@ -1,6 +0,0 @@
-//front_client\lib\core\constants.dart
-
-const double kDefaultPadding = 16.0;
-const double kAvatarRadius = 40.0;
-const String kDefaultChildAvatarUrl = 'https://via.placeholder.com/100';
-const int kPageSize = 20;
